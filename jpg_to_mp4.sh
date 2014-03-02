be3a72a71bc4913bfbaacf3e2ff4875f53681a03
Steven D. Patz <steven.patz@gmail.com>
2014-03-01 23:37:41 -0500

    First variable on the command line is the descriptive name of the video, then the date, and then if needed a part descriptor.

--- jpg_to_mp4.sh
+++ jpg_to_mp4.sh
@@ -1,11 +1,8 @@
-# Sort pictures from GOPRO09* in img (symbolic links)
-# TODO: Make all file and directory calls dynamic.
-
+date=`date +%F`
 x=1;for i in G*.JPG; do counter=$(printf %06d $x); ln "$i" img_in_order/img"$counter".jpg; x=$(($x+1)); done
  
 # Rotate pictures if needed
 #for file in *.jpg; do convert $file -rotate 180 rotated-$file; done
  
-# Make a 24img/s movie
-ffmpeg -i "img_in_order/img%06d.jpg" -r 24 -s hd720 -vcodec libx264 -crf 18 -threads 8 NAME.mp4
+ffmpeg -i "img_in_order/img%06d.jpg" -r 24 -s hd720 -vcodec libx264 -crf 18 -threads 8 $1_$date_$2.mp4
 
